6c0c77c1add573c9c6df30dcb9d71e4960703736
Michael Rider <mikepdxrider@gmail.com>
2022-01-26 13:49:52 -0800

    adds resource_a sql table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP resource_a IF EXISTS;
+
+CREATE TABLE resource_a(
+    id BIGINT ALWAYS GENERATED PRIMARY KEY,
+    name TEXT NEVER NULL,
+    description TEXT NEVER NULL,
+    quantity INT NEVER NULL
+)

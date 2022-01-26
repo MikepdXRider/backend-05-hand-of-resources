6a838a3e279607583f7c7ad28aaa371b56a37325
Michael Rider <mikepdxrider@gmail.com>
2022-01-26 13:45:40 -0800

    adds resource-a post test

--- __tests__/resource-a.test.js
+++ __tests__/resource-a.test.js
@@ -3,6 +3,12 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+mockResourceA = {
+    name: 'test-resource-a-name',
+    description: 'test-resource-a-description',
+    quantity: 1000000,
+}
+
 describe('backend-05-hand-of-resources resource-a routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -11,4 +17,18 @@ describe('backend-05-hand-of-resources resource-a routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('sends a post request to resource-a and recieves an object in the correct shape', async () => {
+    const response = await app.request('/api/vi/resource-a/').send(mockResourceA);
+
+    const actual = response.body;
+
+    const expected = {
+      ...mockResourceA,
+      id: expect.any(String),
+    }
+
+    expect(actual).toEqual(expected);
+  });
+
 });

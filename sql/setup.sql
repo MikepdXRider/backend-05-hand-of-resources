70ee085e70d6254c9ac54f82554e4b1889fb05ce
Michael Rider <mikepdxrider@gmail.com>
2022-01-27 10:00:43 -0800

    adds resource-d sql table

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,7 @@
 DROP TABLE IF EXISTS resource_a;
 DROP TABLE IF EXISTS resource_b;
 DROP TABLE IF EXISTS resource_c;
+DROP TABLE IF EXISTS resource_d;
 
 CREATE TABLE resource_a (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -23,4 +24,11 @@ CREATE TABLE resource_c (
     warning TEXT NOT NULL,
     time_of_error TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_resolved BOOLEAN NOT NULL
+);
+
+CREATE TABLE resource_d (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    contract_value INT NOT NULL
 );

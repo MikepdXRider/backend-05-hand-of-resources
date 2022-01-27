03aac07f83987a675208d0b246ff7fa121e056db
Michael Rider <mikepdxrider@gmail.com>
2022-01-26 18:22:35 -0800

    adds failing resource-c post test

--- __tests__/resource-c.test.js
+++ __tests__/resource-c.test.js
@@ -3,6 +3,11 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+mockResourceC = {
+  warning: 'test-resource-c-warning',
+  isResolved: false,
+}
+
 describe('backend-05-hand-of-resources resource-c routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -12,7 +17,10 @@ describe('backend-05-hand-of-resources resource-c routes', () => {
     pool.end();
   });
 
-  it.skip('test', () => {
-
+  it('sends a post request to resource-c and recieves an object in the correct shape', async () => {
+    const insertResponse = await request(app).post('/api/v1/resource-c/').send(mockResourceC);
+    const actual = insertResponse.body;
+    
+    expect(actual).toEqual(mockResourceC);
   });
 });
